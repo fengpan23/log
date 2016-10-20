ad8f3b3b3088ad0ac40d051f5c41001e7a17804e
fengpan <pan.feng@stargt.com.my>
2016-10-20 18:33:37 +0800

    use singleton

--- index.js
+++ index.js
@@ -3,46 +3,60 @@
  */
 "use strict";
 
-function Log(opt) {
-    let options = opt;
-    if(options.debug){
-        //    文件 和 即时日志
-    }else if(options.develop){
-    //    即时日志
-    }else if(options.product){
-    //    文件 日志
+function Log(options) {
+    options = options || {};
+    switch (options.env){
+        case 'debug':
+            //    文件 和 即时日志
+            break;
+        case 'develop':
+            //    即时日志
+            break;
+        case 'product':
+            //    文件 日志
+            break;
+        default:
     }
-
-    function L() {
-
-    }
-    L.prototype.info = function () {
-        console.info.apply(console, arguments);
-    };
-
-    L.prototype.error = function () {
-        console.error.apply(console, arguments);
-    };
-
-    let timeRecord = new Map();
-    L.prototype.timeStart = function (name) {
-        timeRecord.set(name, +new Date());
-    };
-    L.prototype.timeEnd = function (name) {
-        if(timeRecord.has(name)){
-            this.info('use time: ' + String(+new Date() - timeRecord.get(name)));
-        }else{
-            this.info('can not find start time name :' + name);
-        }
-    };
-    return global.log = new L();
 }
-module.exports = Log;
+Log.prototype.info = function () {
+    console.info.apply(console, arguments);
+};
+
+Log.prototype.error = function () {
+    console.error.apply(console, arguments);
+};
+
+let timeRecord = new Map();
+Log.prototype.timeStart = function (name) {
+    timeRecord.set(name, +new Date());
+};
+Log.prototype.timeEnd = function (name) {
+    if(timeRecord.has(name)){
+        this.info('use time: ' + String(+new Date() - timeRecord.get(name)));
+    }else{
+        this.info('can not find start time name :' + name);
+    }
+};
+let singleton, log;
+
+/**
+ * log module
+ * @param options   {object} {singleton: Boolean, env: String ['debug' | 'develop' | 'product]}
+ * @returns {Log}
+ */
+module.exports = function(options){
+    if(singleton || options.singleton){
+        singleton = singleton || options.singleton;
+        return log && log.constructor  === Log ? log : new Log(options);
+    }else{
+        return new Log(options);
+    }
+};
 
 //test
 if(require.main !== module) return;
-new Log({});
-log.timeStart('start');
+let l = new Log({});
+l.timeStart('start');
 setTimeout(() => {
-    log.timeEnd('start')
+    l.timeEnd('start')
 }, 2000);
